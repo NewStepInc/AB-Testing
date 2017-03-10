e4dab90540e9c654069a0d20511605b329f55b09
Yonis Larsson <yonis.larsson.biz@gmail.com>
2017-03-11 00:01:53 +0800

    import optimization

--- AB-Testing-master/app/src/main/java/roxma/org/sms_forward/MainActivity.java
+++ AB-Testing-master/app/src/main/java/roxma/org/sms_forward/MainActivity.java
@@ -1,26 +1,17 @@
 package roxma.org.sms_forward;
 
 import android.Manifest;
-import android.annotation.TargetApi;
 import android.app.AlertDialog;
-import android.content.ContentResolver;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.provider.Telephony;
 import android.support.annotation.NonNull;
-import android.support.v13.app.ActivityCompat;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
